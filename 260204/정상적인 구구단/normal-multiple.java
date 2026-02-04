65d4efa534b14621d19c1c0f3e1cad0665e44e67
JayP47 <165177434+JayP47@users.noreply.github.com>
2026-02-04 17:46:21 +0900

    Title: 정상적인 구구단, Time: 137ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260204/정상적인 구구단/normal-multiple.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        
+        for (int i = 1; i <= x; i++) {
+			for (int j = 1; j <= x; j++) {
+				if(j==x) {
+					System.out.printf("%d * %d = %d",i,j,i*j);
+					break;
+				}
+				System.out.printf("%d * %d = %d, ",i,j,i*j);
+			}
+			System.out.println();
+		}
+    }
+}
