790180f15cb30b57e347939e084ae385999911f0
JayP47 <165177434+JayP47@users.noreply.github.com>
2026-03-04 15:03:12 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 320ms, Memory: 21MB, Status: Passed - Codetree

--- 260304/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
+++ 260304/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -1,45 +1,46 @@
-import java.util.Scanner;
+import java.util.*;
+
 public class Main {
-	static int n,m;
-	static int[] row = {0,0,1,-1};
-	static int[] col = {1,-1,0,0};
-	static int[][] grid;
-	static boolean[][] visited;
-	static boolean canGo;
-	
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        grid = new int[n][m];
-        visited = new boolean[n][m];
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] grid = new int[n][m];
+        boolean[][] visited = new boolean[n][m];
+        
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
                 grid[i][j] = sc.nextInt();
+
+        int[] dr = {0, 0, 1, -1};
+        int[] dc = {1, -1, 0, 0};
         
-        findPath(0,0);
-        if(canGo) {
-        	System.out.println(1);
-        } else {
-        	System.out.println(0);
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0, 0});
+        visited[0][0] = true;
+        
+        boolean canGo = false;
+        while(!q.isEmpty()) {
+            int[] curr = q.poll();
+            int r = curr[0];
+            int c = curr[1];
+            
+            if(r == n-1 && c == m-1) {
+                canGo = true;
+                break;
+            }
+            
+            for(int i = 0; i < 4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1 && !visited[nr][nc]) {
+                    visited[nr][nc] = true;
+                    q.offer(new int[]{nr, nc});
+                }
+            }
         }
-    }
-    
-    static void findPath(int r, int c) {
-    	visited[r][c] = true;
-    	if(canGo) {
-    		return;
-    	}
-    	if(r==n-1 && c == m-1) {
-    		canGo = true;
-    		return;
-    	}
-    	for (int i = 0; i < 4; i++) {
-			int nr = r+row[i];
-			int nc = c+col[i];
-			if(0<=nr && nr<n && 0<=nc && nc<m && grid[nr][nc]==1 && !visited[nr][nc]) {
-				findPath(nr,nc);
-			}
-		}
+        
+        System.out.println(canGo ? 1 : 0);
     }
 }
